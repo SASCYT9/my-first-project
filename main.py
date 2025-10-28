2c67d464db0d633c48925953ba7582b54d201a87
SASCYT9 <sashatsompel@gmail.com>
2025-10-28 20:46:30 +0200

    Додали коментар до коду

---
+++ main.py
@@ -0,0 +1,18 @@
+import pyautogui
+from PIL import ImageGrab
+import keyboard
+
+print("Наведіть мишку на потрібне місце і натисніть SPACE")
+
+selected_pos = None
+target_color = None
+
+def on_space():
+    global selected_pos, target_color
+    selected_pos = pyautogui.position()
+    screenshot = ImageGrab.grab()
+    target_color = screenshot.getpixel(selected_pos)
+    print(f"Збережено: позиція {selected_pos}, колір {target_color}")
+
+keyboard.on_press_key("space", lambda _: on_space())
+keyboard.wait("esc")
